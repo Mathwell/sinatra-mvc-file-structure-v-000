5665be95e40ce5b6764d1085a827ff8387127f50
Olena Ageyeva <eageeva@hotmail.com>
2017-12-10 22:55:56 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,14 @@
 class Dog
   attr_accessor :name, :breed, :age
 
+  @@all=[]
+
   def initialize(name,breed, age)
     @name, @breed, @age= name, breed, age
+    @@all<<self
+  end
+
+  def class.all
+    @@all
   end
 end
